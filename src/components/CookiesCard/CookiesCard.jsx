3e4c6c93dc68035a444675374e39063f3240a247
CristinaColomoiets <colomoetscristina@gmail.com>
2024-05-11 18:03:35 +0200

    created buttons in CookieCard

--- src/components/CookiesCard/CookiesCard.jsx
+++ src/components/CookiesCard/CookiesCard.jsx
@@ -1,4 +1,5 @@
-import Card from 'react-bootstrap/Card';
+
+import { Row, Col, Card, Button, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 
@@ -12,11 +13,33 @@ const CookiesCard = ({ name, image_url, brand, id }) => {
                     {brand}
                 </Card.Text>
             </Card.Body>
-            <Card.Body>
-                <Link to={`/cookies/${id}`}>
-                    <Card.Link as="span" href="#">Cookies Details</Card.Link>
-                </Link>
-            </Card.Body>
+            <Container>
+                <Row>
+                    <Col md ={{span: 4}}>
+                        <Card.Body>
+                            <Link to={`/cookies/${id}`}>
+                                <Button href="#">Details</Button>
+                            </Link>
+                        </Card.Body>
+                    </Col>
+
+                    <Col md ={{span: 4}}>
+                        <Card.Body>
+                            <Link to={`/cookies/${id}`}>
+                                <Button href="#">Edit</Button>
+                            </Link>
+                        </Card.Body>
+                    </Col>
+
+                    <Col md ={{span: 4}}>
+                        <Card.Body>
+                            <Link to={`/cookies/${id}`}>
+                                <Button href="#">Delete</Button>
+                            </Link>
+                        </Card.Body>
+                    </Col>
+                </Row>
+            </Container>
         </Card>
     );
 
